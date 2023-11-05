b3f5ba588c8148c777703be312f4a25f0270292b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-05 15:23:12 +0100

    content: change text

--- layouts/PostLayout.tsx
+++ layouts/PostLayout.tsx
@@ -81,7 +81,7 @@ export default function PostLayout({ content, authorDetails, next, prev, childre
             <div className="divide-y divide-gray-200 dark:divide-gray-700 xl:col-span-3 xl:row-span-2 xl:pb-0">
               <div className="prose max-w-none pb-8 pt-10 dark:prose-invert">{children}</div>
               <div className="pb-6 pt-6 text-sm text-gray-700 dark:text-gray-300">
-                <Link href={editUrl(filePath)}>View on GitHub</Link>
+                <Link href={editUrl(filePath)}>View this article on GitHub</Link>
               </div>
               {siteMetadata.comments && (
                 <div
