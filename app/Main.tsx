29d66be8922c31c81975c6c1ce24a9f19381ff1c
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-05 15:23:47 +0100

    config: max display set to 10

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,7 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 
-const MAX_DISPLAY = 5
+const MAX_DISPLAY = 10
 
 export default function Home({ posts }) {
   return (
