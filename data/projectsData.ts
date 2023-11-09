cc33ddb6bd87e3acb0d65dc976b48a98e52f595c
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-09 21:56:16 +0100

    content: add NuGet packages to projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -1,7 +1,7 @@
 const projectsData = [
   {
     title: 'Blog about programming (in Polish)',
-    description: `Previous version of blog about programming, written in Polish. Features the Test-Driven Development course.
+    description: `Previous version of blog about programming, written in Polish. Features the TDD course.
     Now, the blog is inactive but the mirror is still available.`,
     imgSrc: '/static/images/projects/blogv1.png',
     href: 'https://dariuszwozniak.netlify.app/',
@@ -11,6 +11,12 @@ const projectsData = [
     description: `The best-selling book about Test-Driven Development in Poland. Published by Helion, the biggest Polish IT books publisher.`,
     imgSrc: '/static/images/projects/cover.jpg',
     href: 'https://helion.pl/ksiazki/tdd-techniki-programowania-sterowanego-testami-dariusz-wozniak,tddppr.htm',
+  },
+  {
+    title: 'NuGet packages',
+    description: `Some open source contribution to the .NET ecosystem.`,
+    imgSrc: '/static/images/projects/circuit-board.svg',
+    href: 'https://www.nuget.org/profiles/dariuszwozniak',
   },
 ]
 
