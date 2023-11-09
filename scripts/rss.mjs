b3490afc4def366f9b31ee070736c992a409fb2b
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-09 09:03:53 +0100

    fix(rss): format managingEditor and webMaster

--- scripts/rss.mjs
+++ scripts/rss.mjs
@@ -1,11 +1,11 @@
-import { writeFileSync, mkdirSync } from 'fs'
+import {writeFileSync, mkdirSync} from 'fs'
 import path from 'path'
 import GithubSlugger from 'github-slugger'
-import { escape } from 'pliny/utils/htmlEscaper.js'
+import {escape} from 'pliny/utils/htmlEscaper.js'
 import siteMetadata from '../data/siteMetadata.js'
-import tagData from '../app/tag-data.json' assert { type: 'json' }
-import { allBlogs } from '../.contentlayer/generated/index.mjs'
-import { sortPosts } from 'pliny/utils/contentlayer.js'
+import tagData from '../app/tag-data.json' assert {type: 'json'}
+import {allBlogs} from '../.contentlayer/generated/index.mjs'
+import {sortPosts} from 'pliny/utils/contentlayer.js'
 
 const generateRssItem = (config, post) => `
   <item>
@@ -26,8 +26,8 @@ const generateRss = (config, posts, page = 'feed.xml') => `
       <link>${config.siteUrl}/blog</link>
       <description>${escape(config.description)}</description>
       <language>${config.language}</language>
-      <managingEditor>${config.email} (${config.author})</managingEditor>
-      <webMaster>${config.email} (${config.author})</webMaster>
+      <managingEditor>${config.email}${config.author}</managingEditor>
+      <webMaster>${config.email}${config.author}</webMaster>
       <lastBuildDate>${new Date(posts[0].date).toUTCString()}</lastBuildDate>
       <atom:link href="${config.siteUrl}/${page}" rel="self" type="application/rss+xml"/>
       ${posts.map((post) => generateRssItem(config, post)).join('')}
@@ -36,27 +36,27 @@ const generateRss = (config, posts, page = 'feed.xml') => `
 `
 
 async function generateRSS(config, allBlogs, page = 'feed.xml') {
-  const publishPosts = allBlogs.filter((post) => post.draft !== true)
-  // RSS for blog post
-  if (publishPosts.length > 0) {
-    const rss = generateRss(config, sortPosts(publishPosts))
-    writeFileSync(`./public/${page}`, rss)
-  }
-
-  if (publishPosts.length > 0) {
-    for (const tag of Object.keys(tagData)) {
-      const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => GithubSlugger.slug(t)).includes(tag)
-      )
-      const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
-      const rssPath = path.join('public', 'tags', tag)
-      mkdirSync(rssPath, { recursive: true })
-      writeFileSync(path.join(rssPath, page), rss)
+    const publishPosts = allBlogs.filter((post) => post.draft !== true)
+    // RSS for blog post
+    if (publishPosts.length > 0) {
+        const rss = generateRss(config, sortPosts(publishPosts))
+        writeFileSync(`./public/${page}`, rss)
     }
-  }
+
+    if (publishPosts.length > 0) {
+        for (const tag of Object.keys(tagData)) {
+            const filteredPosts = allBlogs.filter((post) => post.tags.map((t) => GithubSlugger.slug(t)).includes(tag)
+            )
+            const rss = generateRss(config, filteredPosts, `tags/${tag}/${page}`)
+            const rssPath = path.join('public', 'tags', tag)
+            mkdirSync(rssPath, {recursive: true})
+            writeFileSync(path.join(rssPath, page), rss)
+        }
+    }
 }
 
 const rss = () => {
-  generateRSS(siteMetadata, allBlogs)
-  console.log('RSS feed generated...')
+    generateRSS(siteMetadata, allBlogs)
+    console.log('RSS feed generated...')
 }
 export default rss
