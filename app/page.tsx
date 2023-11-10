793eb421217a2bdc3d5f7dfe3118c1da52163064
Dariusz Woźniak <dariusz.wozniak@gmail.com>
2023-11-10 22:28:50 +0100

    feat: show pages only published before today

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,17 @@ import { sortPosts, allCoreContent } from 'pliny/utils/contentlayer'
 import { allBlogs } from 'contentlayer/generated'
 import Main from './Main'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default async function Page() {
-  const sortedPosts = sortPosts(allBlogs)
+  let sortedPosts = sortPosts(allBlogs)
+  if (isProduction) {
+    const today = new Date()
+    today.setHours(12, 0, 0, 0)
+    sortedPosts = sortedPosts.filter((post) => {
+      return new Date(post.date) <= today
+    })
+  }
   const posts = allCoreContent(sortedPosts)
   return <Main posts={posts} />
 }
